b97d72437f7f3061cf9acac7ba18b604e0899e77
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-02-04 18:20:49 +0530

    Including date params to analyze API call

--- src/components/pages/analyzeContainer/actions/analyzeContainerActions.js
+++ src/components/pages/analyzeContainer/actions/analyzeContainerActions.js
@@ -1,4 +1,5 @@
 import * as ACTIONS from './analyzeContainerActionTypes';
+import { dateRangeSelector } from '~/src/components/dashBoardDatePicker/selectors/dashboardDatePickerSelectors';
 
 export function loadFail(error) {
   return {
@@ -27,7 +28,13 @@ export function load() {
           type: ACTIONS.LOAD
         });
 
-        return client.get('/api/analyze').then(data => {
+        const state = getState();
+        const dateRange = dateRangeSelector(state);
+        let apiLink = '/api/analyze';
+        if (dateRange.startDate && dateRange.endDate) {
+            apiLink += `?start-date=${dateRange.startDate}&end-date=${dateRange.endDate}`;
+        }
+        return client.get(apiLink).then(data => {
             dispatch(loadSuccess(data.data));
         })
         .catch(error => {
