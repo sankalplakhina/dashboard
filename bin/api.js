52dbab4e1c016e04f76d71c978a8bf9ad98a8170
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-02-05 16:02:12 +0530

    statsPanels dummy API

--- bin/api.js
+++ bin/api.js
@@ -23,7 +23,7 @@ app.get('/api/home', (req, res) => {
 app.get('/api/explore', (req, res) => {
 	res.json({
 		data: {
-			rows: ['quickInfo', 'avgStats'],
+			rows: ['quickInfo', 'avgStats', 'statsPanels'],
 			quickInfo: {
 				cols: ['newOrders', 'riskyOrders'],
 				newOrders: {title: 'New Orders', count: 30},
@@ -67,6 +67,325 @@ app.get('/api/explore', (req, res) => {
 						},
 					]
 				}
+			},
+			statsPanels: {
+				rows: ['graphControls', 'panelData'],
+				graphControls: {
+
+				},
+				panelData: {
+					rows: ['row07SND73H', 'row08SND73H'],
+					panelTitle: {
+						cols: ['score', 'order', 'lastPaymentAbuseStatus'],
+						score: {
+							title : 'SCORE',
+						},
+						order: {
+							title: 'ORDER',
+						},
+						lastPaymentAbuseStatus: {
+							title: 'LAST PAYMENT ABUSE STATUS',
+						},
+					},
+					row07SND73H: {
+						score: {
+							icon: 'score.png',
+							count:  84,
+						},
+						order: {
+							title: '07SND73H',
+							risky: true,
+							paras: ['cockatrice8414', 'Dec 3, 2016 1:10 PM (3 days ago)']
+						},
+						lastPaymentAbuseStatus: {
+							buttonOptions: ['Approve', 'Block']
+						},
+						isExpanded: true,
+						_collapsedData: {
+							cols: ['left', 'right'],
+							left: {
+								rows: ['orders', 'payment', 'billingShipping', 'itemOrdered'],
+								orders: {
+									type: 'table',
+									widthSize: 12,
+									heading: {
+										icon: 'orders.png',
+										title: 'Orders'
+									},
+									tableTitle: {
+										cols: ['orderId', 'amount', 'date', 'payment', 'status', 'attempts'],
+										orderId: {
+											title: 'Order ID'
+										},
+										amount: {
+											title: 'Amount'
+										},
+										date: {
+											title: 'Date'
+										},
+										payment: {
+											title: 'Payment'
+										},
+										status: {
+											title: 'Status'
+										},
+										attempts: {
+											title: 'Attempts'
+										}
+									},
+									rows: [],
+								},
+								payment: {
+									type: 'table',
+									widthSize: 12,
+									heading: {
+										icon: 'payment.png',
+										title: 'Payment'
+									},
+									tableTitle: {
+										cols: ['status', 'txnType', 'amount', 'date', 'payment', 'gateway', 'avs-cvv', 'last4'],
+										status: {
+											title: 'Order ID'
+										},
+										txnType: {
+											title: 'Amount'
+										},
+										amount: {
+											title: 'Date'
+										},
+										date: {
+											title: 'Payment'
+										},
+										payment: {
+											title: 'Status'
+										},
+										attempts: {
+											title: 'Attempts'
+										},
+										'avs-cvv': {
+											title: 'AVS/CVV'
+										},
+										last4: {
+											title: 'Last 4'
+										}
+									},
+									rows: [],
+								},
+								billingShipping: {
+									type: 'map',
+									widthSize: 6,
+									heading: {
+										icon: 'billingShipping.png',
+										title: 'Billing and Shipping'
+									},
+								},
+								itemOrdered: {
+									type: 'table',
+									widthSize: 6,
+									heading: {
+										icon: 'orders.png',
+										title: 'Item Ordered'
+									},
+									tableTitle: {
+										cols: ['item', 'quantity', 'price'],
+										item: {
+											title: 'Item'
+										},
+										quantity: {
+											title: 'Quantity'
+										},
+										price: {
+											title: 'Price'
+										},
+									},
+									rows: [],
+								}
+							},
+							right: {
+								rows: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'],
+								item1: {
+									icon: 'statusReliable.png',
+									text: 'Account age',
+									postText: 'Gurgaon Is Safe Zone : Yes',
+								},
+								item2: {
+									icon: 'statusReliable.png',
+									text: 'Account age',
+									postText: '1 year : Reliable',
+								},
+								item3: {
+									icon: 'statusTooFast.png',
+									text: 'Time to order',
+									postText: '5 min : Too fast, abnormal',
+								},
+								item4: {
+									icon: 'statusNone.png',
+									text: 'IP Address location',
+									postText: 'Delhi : None',
+								},
+								item5: {
+									icon: 'statusReliable.png',
+									text: 'Device Cost',
+									postText: 'Rs 25K : Healthy',
+								},
+								item6: {
+									icon: 'statusTooFast.png',
+									text: 'Email address provider',
+									postText: 'gmail/ht.com : Reliable',
+								},
+							}
+						}
+					},
+					row08SND73H: {
+						score: {
+							icon: 'score.png',
+							count:  85,
+						},
+						order: {
+							title: '08SND73H',
+							risky: true,
+							paras: ['cockatrice8514', 'Dec 4, 2016 1:10 PM (3 days ago)']
+						},
+						lastPaymentAbuseStatus: {
+							buttonOptions: ['Approve', 'Block']
+						},
+						_collapsedData: {
+							cols: ['left', 'right'],
+							left: {
+								rows: ['orders', 'payment', 'billingShipping', 'itemOrdered'],
+								orders: {
+									type: 'table',
+									widthSize: 12,
+									heading: {
+										icon: 'orders.png',
+										title: 'Orders'
+									},
+									tableTitle: {
+										cols: ['orderId', 'amount', 'date', 'payment', 'status', 'attempts'],
+										orderId: {
+											title: 'Order ID'
+										},
+										amount: {
+											title: 'Amount'
+										},
+										date: {
+											title: 'Date'
+										},
+										payment: {
+											title: 'Payment'
+										},
+										status: {
+											title: 'Status'
+										},
+										attempts: {
+											title: 'Attempts'
+										}
+									},
+									rows: [],
+								},
+								payment: {
+									type: 'table',
+									widthSize: 12,
+									heading: {
+										icon: 'payment.png',
+										title: 'Payment'
+									},
+									tableTitle: {
+										cols: ['status', 'txnType', 'amount', 'date', 'payment', 'gateway', 'avs-cvv', 'last4'],
+										status: {
+											title: 'Order ID'
+										},
+										txnType: {
+											title: 'Amount'
+										},
+										amount: {
+											title: 'Date'
+										},
+										date: {
+											title: 'Payment'
+										},
+										payment: {
+											title: 'Status'
+										},
+										attempts: {
+											title: 'Attempts'
+										},
+										'avs-cvv': {
+											title: 'AVS/CVV'
+										},
+										last4: {
+											title: 'Last 4'
+										}
+									},
+									rows: [],
+								},
+								billingShipping: {
+									type: 'map',
+									widthSize: 6,
+									heading: {
+										icon: 'billingShipping.png',
+										title: 'Billing and Shipping'
+									},
+								},
+								itemOrdered: {
+									type: 'table',
+									widthSize: 6,
+									heading: {
+										icon: 'orders.png',
+										title: 'Item Ordered'
+									},
+									tableTitle: {
+										cols: ['item', 'quantity', 'price'],
+										item: {
+											title: 'Item'
+										},
+										quantity: {
+											title: 'Quantity'
+										},
+										price: {
+											title: 'Price'
+										},
+									},
+									rows: [],
+								}
+							},
+							right: {
+								rows: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'],
+								item1: {
+									icon: 'statusReliable.png',
+									text: 'Account age',
+									postText: 'Gurgaon Is Safe Zone : Yes',
+								},
+								item2: {
+									icon: 'statusReliable.png',
+									text: 'Account age',
+									postText: '1 year : Reliable',
+								},
+								item3: {
+									icon: 'statusTooFast.png',
+									text: 'Time to order',
+									postText: '5 min : Too fast, abnormal',
+								},
+								item4: {
+									icon: 'statusNone.png',
+									text: 'IP Address location',
+									postText: 'Delhi : None',
+								},
+								item5: {
+									icon: 'statusReliable.png',
+									text: 'Device Cost',
+									postText: 'Rs 25K : Healthy',
+								},
+								item6: {
+									icon: 'statusTooFast.png',
+									text: 'Email address provider',
+									postText: 'gmail/ht.com : Reliable',
+								},
+							}
+						}
+					}
+				}
+
 			}
 		}
 	});
