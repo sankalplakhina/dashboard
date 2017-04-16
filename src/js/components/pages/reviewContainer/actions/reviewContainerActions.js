77831b58be8108edd57bd86c80a75af2f7a5fa6a
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-04-16 12:50:46 +0530

    Using selectors for defaults in review api

--- src/js/components/pages/reviewContainer/actions/reviewContainerActions.js
+++ src/js/components/pages/reviewContainer/actions/reviewContainerActions.js
@@ -1,6 +1,7 @@
 import * as ACTIONS from './reviewContainerActionTypes';
 import { getDayBucketValue } from 'src/js/components/dashboard/selectors/dashboardDatePickerSelectors';
 import { getUserSecretKey } from 'src/js/components/pages/loginContainer/selectors/loginContainerSelectors';
+import { getReviewApiPath, getReviewApiRowsCount } from '../selectors/reviewContainerSelectors';
 
 export function loadFail(error) {
   return {
@@ -16,7 +17,7 @@ export function loadSuccess(data) {
   };
 }
 
-export function load(apiLink = '/fapi/review?row=5') {
+export function load(apiLink = `${getReviewApiPath()}`) {
     // returning a thunk as this is any async action
     // dispatch and getState and default params from
     // thunk library, client is an extra param required
@@ -31,7 +32,7 @@ export function load(apiLink = '/fapi/review?row=5') {
         const state = getState();
         const secret = getUserSecretKey(state);
         const dayBucket = getDayBucketValue(state);
-        const apiUrl = `${apiLink}&secret=${secret}&days=${dayBucket}`;
+        const apiUrl = `${apiLink}?row=${getReviewApiRowsCount()}&secret=${secret}&days=${dayBucket}`;
         return client.get(apiUrl).then(data => {
             dispatch(loadSuccess(data));
         })
--- src/js/components/pages/reviewContainer/selectors/reviewContainerSelectors.js
+++ src/js/components/pages/reviewContainer/selectors/reviewContainerSelectors.js
@@ -1,7 +1,17 @@
-export const reviewDataLoaded = (state) => {
-  return state.review.loaded;
+import _ from 'lodash';
+
+export const getReviewApiPath = () => '/fapi/review';
+export const getReviewApiRowsCount = () => '5';
+
+export const getReview = (state) => {
+	return state.review;
 };
 
 export const getReviewData = (state) => {
-  return state.review.data;
+	return _.get(getReview(state), 'data');
 };
+
+export const reviewDataLoaded = (state) => {
+	return _.get(getReview(state), 'loaded');
+};
+
