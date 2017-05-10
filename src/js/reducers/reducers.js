4d3ec6ac1ce046462918af32a8c0c85c447c8397
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-05-10 17:03:28 +0530

    Removing related paths in root reducer

--- src/js/reducers/reducers.js
+++ src/js/reducers/reducers.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-connect';
 import auth from 'src/js/components/pages/loginContainer/reducers/loginContainerReducers';
-import { dashboard } from '../components/dashboard/reducers/dashBoardDatePickerReducers';
+import { dashboard } from 'src/js/components/dashboard/reducers/dashBoardDatePickerReducers';
 import explore from '../components/pages/exploreContainer/reducers/exploreContainerReducers';
-import { reducer as review } from '../components/pages/reviewContainer/reducers/reviewContainerReducers';
-import { reducer as analyze } from '../components/pages/analyzeContainer/reducers/analyzeContainerReducers';
+import { reducer as review } from 'src/js/components/pages/reviewContainer/reducers/reviewContainerReducers';
+import { reducer as analyze } from 'src/js/components/pages/analyzeContainer/reducers/analyzeContainerReducers';
 import order from 'src/js/components/pages/order/reducers/orderContainerReducers';
 
 // root reducer
