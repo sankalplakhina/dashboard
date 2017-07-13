4d1c87c9d75d105dc0d1289f2358518ed1aed68e
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-07-13 22:49:29 +0530

    Updating Navbar profile options

--- src/js/components/common/components/navbar.jsx
+++ src/js/components/common/components/navbar.jsx
@@ -1,75 +1,104 @@
-/* eslint react/prefer-stateless-function: 0 */
-
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router';
+import { bindHandlers } from 'react-bind-handlers';
+import cx from 'classnames';
 
 import twLogo from 'public/static/images/twLogo.png';
 
 class Navbar extends React.Component {
-  render() {
 
-  	const { isEnabled, addFakeNavSpace, userInfo, onLogoutClick } = this.props;
+    constructor(){
+        super();
+        this.state = {
+            viewDropdown: false,
+        };
+    }
 
-    if (isEnabled) {
-        return (
-            <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container-fluid nopad">
-                    {/* Brand and toggle get grouped for better mobile display --> */}
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <Link className="navbar-brand navPad" to="/">
-                            <img src={twLogo} />
-                        </Link>
-                    </div>
-
-                    {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
-                    <div className="collapse navbar-collapse nopad" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav navbar-right navPad">
-                            {
-                                userInfo?
-                                <li className="logout-li">
-                                    <a>{`Hello! ${userInfo.name}`}</a>
-                                    <span
-                                        className="logout glyphicon glyphicon-off"
-                                        onClick={onLogoutClick}
-                                        >
-                                    </span>
-                                </li>
-                                :
-                                [
-                                    <li key="login"><Link to="/login" className="loginSap">Login</Link></li>,
-                                    <li key="register"><Link to="/register">Signup</Link></li>
-                                ]
-                            }
-                        </ul>
-                        <div className="col-lg-12 menu navPad">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li><a href="//www.thirdwatch.ai/" target="_blank">Our products</a></li>
-                                <li><a href="//api.thirdwatch.ai/" target="_blank">Docs</a></li>
-                                <li><a href="//www.thirdwatch.ai/" target="_blank">Pricing</a></li>
-                                <li><a href="mailto:hello@thirdwatch.ai" target="_blank">Support</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* <!-- /.navbar-collapse --> */}
-                    {
-                        addFakeNavSpace &&
-                        <div className="navFakeSpace">
-                            <div></div>
-                        </div>
-                    }
-                </div>
-            </nav>
+    handleViewDropdown() {
+        this.setState({
+            viewDropdown: !this.state.viewDropdown
+        }, () =>
+            (this.state.viewDropdown? this.refs.dropdownElm.focus() : '')
         );
     }
-    return null;
-  }
+
+    handleHideDropdown() {
+        this.setState({
+            viewDropdown: false
+        });
+    }
+
+    handleProfileLinkClick() {
+        this.props.router.push('/profile');
+    }
+
+    render() {
+
+    	const { isEnabled, addFakeNavSpace, userInfo, onLogoutClick } = this.props;
+        const { viewDropdown } = this.state;
+
+        if (isEnabled) {
+            return (
+                <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container-fluid nopad">
+                        {/* Brand and toggle get grouped for better mobile display --> */}
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <Link className="navbar-brand navPad" to="/">
+                                <img src={twLogo} />
+                            </Link>
+                        </div>
+
+                        {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+                        <div className="collapse navbar-collapse nopad" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav navbar-right navPad">
+                                {
+                                    userInfo?
+                                    <li className={cx("dropdown", { 'open': viewDropdown })}>
+                                        <a  href="#"
+                                            className="dropdown-toggle"
+                                            onClick={this.handleViewDropdown}>{`Hello! ${userInfo.name}`} <span className="caret"></span>
+                                        </a>
+                                        <ul ref="dropdownElm" className="dropdown-menu" tabIndex="0" onBlur={this.handleHideDropdown}>
+                                            <li><a onMouseDown={this.handleProfileLinkClick }>Profile</a></li>
+                                            <li><a href="#" onMouseDown={onLogoutClick}>Logout</a></li>
+                                        </ul>
+                                    </li>
+                                    :
+                                    [
+                                        <li key="login"><Link to="/login" className="loginSap">Login</Link></li>,
+                                        <li key="register"><Link to="/register">Signup</Link></li>
+                                    ]
+                                }
+                            </ul>
+                            <div className="col-lg-12 menu navPad">
+                                <ul className="nav navbar-nav navbar-right">
+                                    <li><a href="//www.thirdwatch.ai/" target="_blank">Our products</a></li>
+                                    <li><a href="//api.thirdwatch.ai/" target="_blank">Docs</a></li>
+                                    <li><a href="//www.thirdwatch.ai/" target="_blank">Pricing</a></li>
+                                    <li><a href="mailto:hello@thirdwatch.ai" target="_blank">Support</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        {/* <!-- /.navbar-collapse --> */}
+                        {
+                            addFakeNavSpace &&
+                            <div className="navFakeSpace">
+                                <div></div>
+                            </div>
+                        }
+                    </div>
+                </nav>
+            );
+        }
+        return null;
+    }
 }
 
 Navbar.defaultProps = {
@@ -77,4 +106,4 @@ Navbar.defaultProps = {
   isEnabled: true,
 };
 
-export default Navbar;
+export default bindHandlers(Navbar);
